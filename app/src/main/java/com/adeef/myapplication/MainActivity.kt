2fd39a45803885aa44ce4e93091c770e613183c5
Ramesh Adeef <m.a.r.adeef@gmail.com>
2024-02-23 00:52:55 -0500

    Added animation

--- app/src/main/java/com/adeef/myapplication/MainActivity.kt
+++ app/src/main/java/com/adeef/myapplication/MainActivity.kt
@@ -25,25 +25,30 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.adeef.myapplication.ui.theme.MyApplicationTheme
+import com.adeef.myapplication.wolf.ScreenWolfApp
+import com.adeef.myapplication.wolf.theme.WoofTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+//            MyApplicationTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
 //                    ScreenTipCalculator(modifier = Modifier
 //                        .fillMaxSize()
 //                        .wrapContentSize(Alignment.Center)
 //                    )
                    /// ScreenAffirmationsApp()
-                    ScreenGridApp()
-                }
-            }
+                   // ScreenGridApp()
+                    WoofTheme{
+                        ScreenWolfApp()
+                    }
+  //              }
+  //          }
         }
     }
 }
